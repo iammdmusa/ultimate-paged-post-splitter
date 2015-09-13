048a779da54107778e8715cf4782aa3a7ccf5325
Md Musa <iammdmusa@users.noreply.github.com>
2015-09-14 02:10:03 +0600

    Update ultimate-paged-post-splitter.php

--- ultimate-paged-post-splitter.php
+++ ultimate-paged-post-splitter.php
@@ -4,12 +4,12 @@
  * User: Md.Musa
  * Date: 2/14/2015
  * Time: 9:07 PM
-Plugin Name:	Ultimate Paged Post splitter
-Description:	The Content Splitter for page & post both.This Plugin is Recommended for Large scale type post or page content and Tutorials blogs or News site.
-Plugin URI:
-Author:			Md Musa
-Author URI:		http://shuvomusa.me
-Version:		1.0.0
+ *   Plugin Name:	Ultimate Paged Post splitter
+ *  Description:	The Content Splitter for page & post both.This Plugin is Recommended for Large scale type post or page content and Tutorials blogs or News site.
+ *   Plugin URI:
+ *  Author:			Md Musa
+ * Author URI:		http://shuvomusa.me
+ *  Version:		1.0.0
 */
 
 $currentFile = __FILE__;
